497a14e13dd6f0b3bfd5d72557146be97ce05d61
Psype <github@arnaud.so>
2016-04-28 16:42:13 +0200

    Cell parse

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <string>
 #include <cstring>
+#include <sstream>
 #include <fstream>
 #include <iostream>
 #include <set>
@@ -11,45 +13,23 @@ const char* const CSV_DELIMITER = ",";
 const int MAX_BUFF_LINE = 12000;
 const int MAX_ELEM = 6000;
 
-void stock(char* tempTab[])
-{
-	for (int i = 0; tempTab[i]; i++)
-      cout << "data[" << i << "] = " << tempTab[i] << endl;
-    cout << endl;
-}
-
 int main(int ac, char* av[])
 {
-	if (ac == 2 && av[1])
+	if (ac == 2)
 	{
-		ifstream file;
-		file.open(av[1]);
-		if (file.good())
-		{
-			while (!file.eof())
-			{
-				char buff[MAX_BUFF_LINE];
-				file.getline(buff, MAX_BUFF_LINE);
-                char* tempTab[MAX_ELEM];
-				tempTab[0] = strtok(buff, CSV_DELIMITER);
-
-				if (tempTab[0])
-				{
-					for (int i = 1; i < MAX_ELEM; i++)
-					{
-						tempTab[i] = strtok(0, CSV_DELIMITER);
-						if (!tempTab[i])
-							break;
-					}
-				}
-			stock(tempTab);
-			}
-		}
-		else
-		{
-			puts("Failed at opening file");
-			return 1;
-		}
+	    ifstream  data(av[1]);
+		string line;
+        while(getline(data,line))
+        {
+            stringstream lineStream(line);
+            string cell;
+            while(getline(lineStream,cell,','))
+            {
+                cout  << cell << endl;
+            }
+        }
 	}
+	else
+        return 1;
 	return 0;
 }
